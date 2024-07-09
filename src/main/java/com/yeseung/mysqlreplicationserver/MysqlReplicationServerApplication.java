4d9a2332bae0a6902433dc3d629a49ea910467a8
이승구 <leesg107@naver.com>
2024-07-09 10:25:33 +0900

    feat: first commit

---
+++ src/main/java/com/yeseung/mysqlreplicationserver/MysqlReplicationServerApplication.java
@@ -0,0 +1,13 @@
+package com.yeseung.mysqlreplicationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlReplicationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlReplicationServerApplication.class, args);
+    }
+
+}
